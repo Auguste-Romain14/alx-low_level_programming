6395300768522431f603916dce643e5208a22b87
Ako Lath Auguste Romain <te.lauguste@gmail.com>
2022-09-08 07:37:09 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- *
  * main - print the size of various types
- * Return: 0
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
-
 {
 	char c;
 	int i;
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
